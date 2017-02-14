c074fadb88895473083c94e6087ef73596313b82
EGOIST <0x142857@gmail.com>
2017-02-15 00:16:08 +0800

    tweaks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -58,7 +58,7 @@
       <h1>Almost There!</h1>
       <a href="https://github.com/egoist/vbuild" class="btn"><svg id="i-book" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6.25%">
     <path d="M16 7 C16 7 9 1 2 6 L2 28 C9 23 16 28 16 28 16 28 23 23 30 28 L30 6 C23 1 16 7 16 7 Z M16 7 L16 28" />
-</svg> Checkout on GitHub</a>
+</svg> Check out on GitHub</a>
     </section>
   </div>
 </template>
